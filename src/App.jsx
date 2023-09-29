6770a8d3214e47d3531fff1b4f49564a7dc004f3
yash8885 <ykakadiya190@gmail.com>
2023-09-29 12:22:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch,useSelector } from 'react-redux'
-import {Add,Remove} from './Action'
+import {Add,Remove} from './Redux/Action'
 
 function App() {
   const count =useSelector((store)=>store)
   const Dispatch=useDispatch()
 
   return (
-    <>
+    <div>
      <h1>Counter:{count}</h1>
      <button onClick={()=>Dispatch(Add())}>ADD</button>
      <button disabled={count==0} onClick={()=>Dispatch(Remove())}>REMOVE</button>
-    </>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { Store } from './Store.js'
+import { Store } from './Redux/Store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
